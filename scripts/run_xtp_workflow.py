ef6af24d8c7d6b36846a1b789866338501275b14
felipe <tifonzafel@gmail.com>
2018-10-12 13:32:29 +0200

    command line parser accept yaml file

--- scripts/run_xtp_workflow.py
+++ scripts/run_xtp_workflow.py
@@ -10,21 +10,13 @@ def cli():
     # Add toil arguments to parsers
 
     parser.add_argument(
-        "--votcashare", help="Path to votca share folder", required=True)
-    parser.add_argument(
-        "--system", help="path to the system description file in xml",
-        default="system.xml")
-    parser.add_argument(
-        "--tpr", help="path to topology file", default="topol.tpr")
-    parser.add_argument(
-        "--gro", help="path to the trajectory file", default="conf.gro")
-
+        "--input", help="Path to the input file (YAML format)", required=True)
     parser.add_argument(
         "--workdir", help=("Work directory"), default='.')
 
     # read command line args
     args = parser.parse_args()
-    opts = ['votcashare', 'system', 'tpr', 'gro', 'workdir']
+    opts = ['input', 'workdir']
 
     return {key: getattr(args, key) for key in opts}
 
