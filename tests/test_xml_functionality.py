dc9547506edba8002cd7723da6d1e72b454ad42b
felipe <tifonzafel@gmail.com>
2019-01-08 15:18:20 +0100

    added xml tests files #2

---
+++ tests/test_xml_functionality.py
@@ -0,0 +1,48 @@
+from xtp_job_control.xml_editor import (
+    edit_xml_job_file, edit_xml_options, read_available_jobs)
+from pathlib import Path
+import shutil
+import xml.etree.ElementTree as ET
+
+
+def copy_to_tmp(origin: Path, dest: Path) -> Path:
+    """
+    Copy `origin` to temp `dest`
+    """
+    shutil.copy(origin, dest)
+
+    return dest / origin.name
+
+
+def read_xml_val(path_file: Path, prop_path: str) -> str:
+    """
+    read the `prop_path` from xml `path_file`
+    """
+    tree = ET.parse(path_file)
+    root = tree.getroot()
+    node = root.find(prop_path)
+
+    return node.text
+
+
+def test_xml_job_file(tmp_path):
+    """ Test that the XML job file is tested correctly"""
+
+    file_path = copy_to_tmp(Path("tests/test_files/eqm.jobs"), tmp_path)
+    rs = edit_xml_job_file(file_path, [1, 2, 3])
+
+    assert len(read_available_jobs(rs)) == 3
+
+
+def test_xml_options(tmp_path):
+    """
+    Test that xml options are edited properly
+    """
+    sections_to_edit = {
+        "neighborlist": {
+            "constant": 0.6}
+    }
+    path_file = copy_to_tmp(Path("tests/test_files/neighborlist.xml"), tmp_path)
+    edit_xml_options(sections_to_edit, tmp_path)
+
+    assert read_xml_val(path_file, "neighborlist/constant") == "0.6"
