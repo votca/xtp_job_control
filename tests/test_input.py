4229cd7d5f6a813d3d2c1825eef4914d8df3a07b
felipe <tifonzafel@gmail.com>
2019-01-22 15:45:50 +0100

    added more testing to input validation function

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,5 +1,6 @@
 from os.path import join
 from xtp_job_control.input import validate_input
+from typing import Dict
 import yaml
 
 
@@ -19,7 +20,21 @@ def test_input_validation(tmp_path):
         "mp_files": "tests/Methane/MP_FILES",
         "qc_files": "tests/Methane/QC_FILES"
     }
+
+    # check valid input
+    dump_yaml(test_file, d)
+    validate_input(test_file.as_posix())
+
+    # check invalid input
+    try:
+        d['system'] = "non-existing/file.xml"
+        dump_yaml(test_file, d)
+        validate_input(test_file)
+    except RuntimeError:
+        # expected error
+        pass
+
+
+def dump_yaml(test_file: str, d: Dict) -> None:
     with open(test_file.as_posix(), 'w')as f:
         f.write(yaml.dump(d))
-
-    validate_input(test_file.as_posix())
