7c479fa9974a0187b0bcad102f6a59581764ae14
felipe <tifonzafel@gmail.com>
2019-01-22 15:57:34 +0100

    check xml job creation

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,5 +1,6 @@
 from xtp_job_control.runner import run
-from xtp_job_control.worflow_components import Results
+from xtp_job_control.worflow_components import (Results, create_workdir, create_xml_job_file)
+from xtp_job_control.xml_editor import read_available_jobs
 from noodles import gather_dict
 
 
@@ -13,3 +14,16 @@ def test_results():
     output = run(gather_dict(**rs.state))
 
     assert output["foo"] == 1
+
+
+def test_xml_job_creation(tmp_path):
+    """
+    Check that a job file is created correctly
+    """
+    workdir = create_workdir(tmp_path, "random")
+
+    jobs = read_available_jobs("tests/test_files/eqm.jobs")
+
+    file_path = create_xml_job_file(jobs[0], workdir)
+
+    assert file_path.exists()
