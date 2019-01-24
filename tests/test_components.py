5d59952beefebbaa50231977cdf5671f88170687
felipe <tifonzafel@gmail.com>
2019-01-24 11:13:23 +0100

    add to_posix test

--- tests/test_components.py
+++ tests/test_components.py
@@ -3,8 +3,9 @@ from xtp_job_control.input import validate_input
 from xtp_job_control.runner import run
 from xtp_job_control.worflow_components import (Results, create_xml_job_file)
 from xtp_job_control.xml_editor import read_available_jobs
-from xtp_job_control.xtp_workflow import (initial_config, recursively_create_path)
+from xtp_job_control.xtp_workflow import (initial_config, recursively_create_path, to_posix)
 from noodles import gather_dict
+import os
 import pytest
 import sys
 import tempfile
@@ -32,6 +33,16 @@ def test_xml_job_creation():
     file_path = create_xml_job_file(jobs[0], Path(tmp_path))
 
     assert file_path.exists()
+
+
+def test_posix():
+    file_path = "tests/test_files/votca/xml"
+    p = Path(file_path)
+    xs = list(p.rglob("*.xml"))
+    d = to_posix({x.name: x for x in xs})
+
+    assert all(isinstance(x, str) for x in to_posix(xs))
+    assert all(os.path.exists(x) for x in d.values())
 
 
 @pytest.mark.skipif(sys.version_info.minor < 6, reason="python 3.6 or higher required")
