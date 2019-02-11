d253ca9cb04b07379d62b27e07aa3242d0d75c5c
felipe <tifonzafel@gmail.com>
2019-02-11 18:30:45 +0100

    defined kmc workflow #4

--- xtp_job_control/workflows/kmc.py
+++ xtp_job_control/workflows/kmc.py
@@ -2,7 +2,6 @@ from ..results import Results
 from ..runner import run
 from .xtp_workflow import (create_promise_command, write_output)
 from .workflow_components import call_xtp_cmd
-from noodles import gather_dict
 
 
 def kmc_workflow(options: dict) -> object:
@@ -17,13 +16,20 @@ def kmc_workflow(options: dict) -> object:
 
     # Run KMC multiple
     kmcmultiple_file = path_optionfiles / "kmcmultiple.xml"
-    args = create_promise_command(
+    args1 = create_promise_command(
         "xtp_run - e kmcmultiple -o {} -f {}", kmcmultiple_file, options["state_file"])
 
-    results["job_kmcmultiple"] = call_xtp_cmd(args, workdir, expected_output={})
+    results["job_kmcmultiple"] = call_xtp_cmd(args1, workdir / "kmcmultiple", expected_output={})
+
+    # Run KMC lifetime
+    kmclifetime_file = path_optionfiles / "kmclifetime.xml"
+    args2 = create_promise_command(
+        "xtp_run - e kmclifetime -o {} -f {}", kmclifetime_file, options["state_file"])
+
+    results["job_kmlifetime"] = call_xtp_cmd(args2, workdir / "kmclifetime", expected_output={})
 
     output = run(results)
     write_output(output)
 
     # xtp_run -e kmcmultiple -o OPTIONFILES/kmcmultiple.xml -f state.sql
-    # xtp_run -e kmclifetime -o OPTIONFILES/kmclifetime.xml -f state.sql 
+    # xtp_run -e kmclifetime -o OPTIONFILES/kmclifetime.xml -f state.sql
